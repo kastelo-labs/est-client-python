242ef7e95b325d8111d5b5e7b9fe1255b9a650c7
Jakob Borg <jakob@kastelo.net>
2020-09-21 17:22:24 +0200

    Avoid charset issue on README

--- setup.py
+++ setup.py
@@ -6,13 +6,6 @@ EST Client
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name = "est",
     version = "0.2.1",
@@ -27,7 +20,7 @@ setup(
         'asn1crypto',
         'requests'
     ],
-    long_description=read('README.md'),
+    long_description='Client to interact with an EST server - RFC 7030.',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
